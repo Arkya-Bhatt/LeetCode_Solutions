f472b57eb5e3c792b11933dcd44ae3633f0e7717
Arkya Bhatt <arkya1509@gmail.com>
2025-10-05 22:39:57 +0530

    107 commit

---
+++ 107-Binary-Tree-Level-Order-Traversal-II/107-README.md
@@ -0,0 +1,42 @@
+# 107. Binary Tree Level Order Traversal II
+
+**Link:** https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+
+**Difficulty:** Medium
+
+---
+
+## Problem Statement
+
+Given the `root` of a binary tree, return _the bottom-up level order traversal of its nodes' values_. (i.e., from left to right, level by level from leaf to root).
+
+A **leaf** is a node with no children.
+
+---
+
+## Examples
+
+**Example 1:**
+
+![alt text](tree1.jpg) \
+**Input:** root = [3,9,20,null,null,15,7] \
+**Output:** [[15,7],[9,20],[3]]
+
+**Example 2:**
+
+**Input:** root = [[1]] \
+**Output:** [[1]]
+
+**Example 3:**
+
+**Input:** root = [] \
+**Output:** []
+
+---
+
+## Constraints
+
+- The number of nodes in the tree is in the range `[0, 2000]`.
+- `-1000 <= Node.val <= 1000`
+
+---
---
+++ 107-Binary-Tree-Level-Order-Traversal-II/binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,27 @@
+from typing import List, Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        queue = [root]
+        result = []
+        while queue:
+            level = []
+            next_queue = []
+            for node in queue:
+                level.append(node.val)
+                if node.left:
+                    next_queue.append(node.left)
+                if node.right:
+                    next_queue.append(node.right)
+            result.append(level)
+            queue = next_queue
+        return result[::-1]
+        
