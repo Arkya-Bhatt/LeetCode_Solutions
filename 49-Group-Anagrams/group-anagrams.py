11cb36337cce9780615953ec8cabb72eb1551126
Arkya Bhatt <arkya1509@gmail.com>
2025-10-07 22:07:40 +0530

    49 commit

---
+++ 49-Group-Anagrams/group-anagrams.py
@@ -0,0 +1,12 @@
+from typing import List
+
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_groups = defaultdict(list)
+        for s in strs:
+            sorted_key = "".join(sorted(s))
+            anagram_groups[sorted_key].append(s)
+        return list(anagram_groups.values())
+        
