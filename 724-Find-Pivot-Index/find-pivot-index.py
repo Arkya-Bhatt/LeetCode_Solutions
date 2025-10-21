abd25ee40b5daf9e968a2389c77d1ec7e57f2c14
Arkya Bhatt <arkya1509@gmail.com>
2025-10-21 11:46:01 +0530

    724 code update commit

---
+++ 724-Find-Pivot-Index/find-pivot-index.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        leftSum = 0
+        for i, num in enumerate(nums):
+            if leftSum == (total - leftSum - num):
+                return i
+            leftSum += num
+        return -1
+        
