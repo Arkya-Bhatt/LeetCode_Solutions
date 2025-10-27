81952fe823564020307202640216ade9f94a771e
Arkya Bhatt <arkya1509@gmail.com>
2025-10-27 23:48:33 +0530

    2197 code update commit

--- 2197-Replace-Non-Coprime-Numbers-in-Array/replace-non-coprime-numbers-in-array.py
+++ 2197-Replace-Non-Coprime-Numbers-in-Array/replace-non-coprime-numbers-in-array.py
@@ -1,2 +1,21 @@
 from typing import List
+import math
 
+class Solution:
+    def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
+        stack = []
+        for num in nums:
+            stack.append(num)
+            while len(stack) >= 2:
+                a = stack[-2]
+                b = stack[-1]
+                g = math.gcd(a, b)
+                if g > 1:
+                    lcm = a * b // g
+                    stack.pop()
+                    stack.pop()
+                    stack.append(lcm)
+                else:
+                    break
+        return stack
+        
