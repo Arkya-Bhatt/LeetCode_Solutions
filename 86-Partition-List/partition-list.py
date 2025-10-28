c969e427379bb921a6d3ef70a01730e0705eea01
Arkya Bhatt <arkya1509@gmail.com>
2025-10-28 23:18:09 +0530

    86 commit

---
+++ 86-Partition-List/partition-list.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        left_dummy = ListNode(0)
+        right_dummy = ListNode(0)
+        left_tail = left_dummy
+        right_tail = right_dummy
+        
+        while head:
+            if head.val < x:
+                left_tail.next = head
+                left_tail = left_tail.next
+            else:
+                right_tail.next = head
+                right_tail = right_tail.next
+            head = head.next
+        
+        right_tail.next = None
+        left_tail.next = right_dummy.next
+        return left_dummy.next
