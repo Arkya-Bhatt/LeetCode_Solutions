1e6c9bedf9a83d1629051d1cbc461d816129f5db
Arkya Bhatt <arkya1509@gmail.com>
2025-11-01 15:09:44 +0530

    3217 commit

---
+++ 3217-Delete-Nodes-From-Linked-List-Present-in-Array/delete-nodes-from-linked-list-present-in-array.py
@@ -0,0 +1,20 @@
+from typing import List, Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        nums = set(nums)
+        temp = ListNode(next=head)
+        prev, curr = temp, head
+        while curr:
+            if curr.val in nums:
+                prev.next = curr.next
+            else:
+                prev = curr
+            curr = curr.next
+        return temp.next
+        
