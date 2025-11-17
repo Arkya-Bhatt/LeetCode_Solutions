3a57b62e189da1659b85013aa79c04f52efcc2cc
Arkya Bhatt <arkya1509@gmail.com>
2025-11-17 08:42:07 +0530

    1437 commit

---
+++ 1437-Check-If-All-1s-Are-at-Least-Length-K-Places-Away/check-if-all-1s-are-at-least-length-k-places-away.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        prev_one_idx = float("-inf")
+        for i, num in enumerate(nums):
+            if num == 1:
+                if (i - prev_one_idx - 1) < k:
+                    return False
+                prev_one_idx = i
+        return True
+        
