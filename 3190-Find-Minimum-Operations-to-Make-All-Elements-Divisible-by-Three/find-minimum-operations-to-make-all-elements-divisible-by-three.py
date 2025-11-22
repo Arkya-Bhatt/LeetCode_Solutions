da0396fcacf464aabbeb389ccf6415867c8338a7
Arkya Bhatt <arkya1509@gmail.com>
2025-11-22 20:48:19 +0530

    3190 commit

---
+++ 3190-Find-Minimum-Operations-to-Make-All-Elements-Divisible-by-Three/find-minimum-operations-to-make-all-elements-divisible-by-three.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        total = 0
+        for n in nums:
+            if n % 3 != 0:
+                total += 1
+        return total
