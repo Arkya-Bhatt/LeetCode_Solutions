25a245a54db121daac72408a7b448510ba353d44
Arkya Bhatt <arkya1509@gmail.com>
2025-12-01 20:19:10 +0530

    976 code update commit

--- 976-Largest-Perimeter-Triangle/largest-perimeter-triangle.py
+++ 976-Largest-Perimeter-Triangle/largest-perimeter-triangle.py
@@ -1,1 +1,16 @@
 from typing import List
+
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        def valid_triangle(a, b, c):
+            if (a + b > c) and (b + c > a) and (c + a > b):
+                return True
+            return False
+        max_perimeter = 0
+        nums.sort(reverse = True)
+        n = len(nums)
+        for i in range(n - 2):
+            if valid_triangle(nums[i], nums[i + 1], nums[i + 2]):
+                max_perimeter = max(max_perimeter, nums[i] + nums[i + 1] + nums[i + 2])
+        return max_perimeter
+        
