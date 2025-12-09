6c291ec97ed7bc3dfac064b27b6a0f94a7ba4474
Arkya Bhatt <arkya1509@gmail.com>
2025-12-09 09:46:19 +0530

    3583 commit

---
+++ 3583-Count-Special-Triplets/count-special-triplets.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def specialTriplets(self, nums: List[int]) -> int:
+        MOD = (10 ** 9) + 7
+        n = len(nums)
+        if n < 3:
+            return 0
+        max_val = max(nums) * 2
+        freqPrev = [0] * (max_val + 1)
+        freqNext = [0] * (max_val + 1)
+        for x in nums:
+            freqNext[x] += 1
+        ans = 0
+        for x in nums:
+            freqNext[x] -= 1
+            t = x * 2
+            ans = (ans + freqPrev[t] * freqNext[t]) % MOD
+            freqPrev[x] += 1
+        return ans
+        
