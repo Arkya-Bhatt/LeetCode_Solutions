442f1040af8f1fd59c74b9e15406e41155b6c750
Arkya Bhatt <arkya1509@gmail.com>
2026-01-05 22:57:35 +0530

    1975 commit

---
+++ 1975-Maximum-Matrix-Sum/maximum-matrix-sum.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
+        total = 0
+        min_abs = float('inf')
+        neg_count = 0
+        for row in matrix:
+            for val in row:
+                if val < 0:
+                    neg_count += 1
+                abs_val = abs(val)
+                total += abs_val
+                if abs_val < min_abs:
+                    min_abs = abs_val
+        if neg_count % 2 == 0:
+            return total
+        else:
+            return total - 2 * min_abs
+        
