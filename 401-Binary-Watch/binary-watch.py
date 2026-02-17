8c87b7ab8df9095ce90a56551d1c9d88b3304792
Arkya Bhatt <arkya1509@gmail.com>
2026-02-17 16:24:29 +0530

    401 commit

---
+++ 401-Binary-Watch/binary-watch.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
+        def count_bits(n: int) -> int:
+            return bin(n).count('1')
+        
+        res = []
+        for h in range(12):
+            for m in range(60):
+                if count_bits(h) + count_bits(m) == turnedOn:
+                    res.append(f"{h}:{m:02d}")
+        return res
+        
