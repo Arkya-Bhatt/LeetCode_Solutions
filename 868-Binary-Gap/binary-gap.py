aa5cec0de6032a78dbb3e8d17bdc8389c01b39d1
Arkya Bhatt <arkya1509@gmail.com>
2026-02-22 10:16:49 +0530

    868 commit

---
+++ 868-Binary-Gap/binary-gap.py
@@ -0,0 +1,12 @@
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        b = bin(n)[2:]
+        last = -1
+        max_gap = 0
+        for i in range(len(b)):
+            if b[i] == '1':
+                if last != -1:
+                    max_gap = max(max_gap, i - last)
+                last = i
+        return max_gap
+        
