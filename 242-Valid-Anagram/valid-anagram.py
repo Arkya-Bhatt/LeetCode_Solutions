cbf080b5a6f93affd28f1821df964a8bdb399515
Arkya Bhatt <arkya1509@gmail.com>
2026-03-23 10:36:27 +0530

    242 commit

---
+++ 242-Valid-Anagram/valid-anagram.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count = defaultdict(int)
+        for ch in s:
+            count[ch] += 1
+        for ch in t:
+            count[ch] -= 1
+            if count[ch] < 0:
+                return False
+        return True
+        
