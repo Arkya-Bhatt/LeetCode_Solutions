83cef027a1b91b875c463afcda5f4ba4f45981bf
Arkya Bhatt <arkya1509@gmail.com>
2026-04-16 17:07:08 +0530

    349 commit

---
+++ 349-Intersection-of-Two-Arrays/intersection-of-two-arrays.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        set1 = set(nums1)
+        res = set()
+        for n in nums2:
+            if n in set1:
+                res.add(n)
+        return list(res)
+        
