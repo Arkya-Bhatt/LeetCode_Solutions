8756d9197bc28121f33941575ae9dc6d9194f3c4
Arkya Bhatt <arkya1509@gmail.com>
2026-05-31 18:54:33 +0530

    2126 commit

---
+++ 2126-Destroying-Asteroids/destroying-asteroids.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        for a in sorted(asteroids):
+            if mass >= a:
+                mass += a
+            else:
+                return False
+        return True
+        
