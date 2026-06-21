8e6d35f6e6d9508e6f26d1ab5f05d384c3572f20
Arkya Bhatt <arkya1509@gmail.com>
2026-06-21 13:10:30 +0530

    1833 commit

---
+++ 1833-Maximum-Ice-Cream-Bars/maximum-ice-cream-bars.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        count = 0
+        for cost in costs:
+            if cost <= coins:
+                count += 1
+                coins -= cost
+            else:
+                break
+        return count
+        
