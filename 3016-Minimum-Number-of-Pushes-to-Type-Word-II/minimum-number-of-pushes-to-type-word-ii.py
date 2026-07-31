56a366d299141d52b60c70ba0b1fc7042189ef8d
Arkyajit Bhattacharjee <arkya1509@gmail.com>
2026-07-31 22:32:07 +0530

    3016 commit

---
+++ 3016-Minimum-Number-of-Pushes-to-Type-Word-II/minimum-number-of-pushes-to-type-word-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        from collections import Counter
+        freqs = sorted(Counter(word).values(), reverse = True)
+        ans = 0
+        for i, f in enumerate(freqs):
+            ans += f * (i // 8 + 1)
+        return ans
+        
